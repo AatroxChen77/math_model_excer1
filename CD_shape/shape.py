b5cfe50255c690ce560b6d4e7e4ee2153540ebc3
aatroxchen <792688960@qq.com>
2023-08-02 00:16:56 +0800

    feat:地区分布武侯区,锦江区

--- CD_shape/shape.py
+++ CD_shape/shape.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   shape.py
+@Time    :   2023/08/01
+@Author  :   ChenYiTao 
+@Version :   3.9.13 64-bit
+@Desc    :   地区分布，不分时间
+'''
+
+# 模块导入
+
 import shapefile
 import matplotlib.pyplot as plt
 import pandas as pd
 import shapely.geometry as geometry
 import warnings
+import time
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
@@ -21,9 +34,23 @@ shapes = sf.shapes() # .shapes()读取几何数据信息，存放着该文件中
 records = sf.records() # .records()读取属性数据信息，存放着该文件中所有对象的 属性数据
 area_num = len(shapes) # 区域数量
 fig, ax = plt.subplots()  # 生成一张图和一张子图
-
 trip_df = pd.read_pickle("GPS_data/trip_df.pkl") # 读取出行数据
+wuhou_id = -1
+jinjiang_id = -1
+# 查找records[i][1]为'武侯区'或'锦江区'的id
+for i in range(area_num):
+    if records[i][1]=='武侯区':
+        wuhou_id = i
+    elif records[i][1]=='锦江区':
+        jinjiang_id = i
+wuhou_shapes = shapes[wuhou_id]
+jinjiang_shapes = shapes[jinjiang_id]
 
+# 初始化 trip_df_wuhou 和 trip_df_jinjiang 为与trip_df相同的格式
+trip_df_wuhou = pd.DataFrame(columns=trip_df.columns)
+trip_df_jinjiang = pd.DataFrame(columns=trip_df.columns)
+
+# 函数定义
 def process_trip(trip_id):
     """
     判断trip_df中的每个trip是否在某个区县内
@@ -38,9 +65,11 @@ def process_trip(trip_id):
         if geometry.Point(trip_df.iloc[trip_id]['start_longitude'], trip_df.iloc[trip_id]['start_latitude']).within(geometry.shape(shapes[j])):
             return j
     return -1
-        
-if __name__ == '__main__':
 
+def count_trips():
+    """
+    统计每个区县的出行次数
+    """
     for i in range(area_num): # 画出成都市所有的区县
         pts = shapes[i].points # 获取每个区县的所有点
         x,y = zip(*pts) # 将所有点的x坐标和y坐标分别提取出来
@@ -59,6 +88,7 @@ if __name__ == '__main__':
 
     # 判断trip_df中的每个trip是否在某个区县内
     area = [0]*len(shapes) # 用于存放每个区县的出行次数
+    start_time = time.time()
     # 遍历每个trip,这一段可以用多进程优化
     with ProcessPoolExecutor(max_workers=worker_num) as executor:
         futures = [executor.submit(process_trip, trip_id) for trip_id in range(len(trip_df))]
@@ -66,6 +96,8 @@ if __name__ == '__main__':
             area_id = future.result()
             if area_id!=-1:
                 area[area_id]+=1
+    end_time = time.time()
+    print('time cost: {:.2f}s'.format(end_time-start_time))
 
     # 保存area
     pd.to_pickle(area, 'CD_shape/area.pkl')
@@ -79,3 +111,52 @@ if __name__ == '__main__':
     plt.show()
     plt.savefig('CD_shape/area.png', dpi=300,bbox_inches='tight') # 保存图片
 
+def process_trip_dis(trip_id):
+    """
+    判断trip_df中的每个trip在武侯区还是锦江区中
+    Args:
+        trip_id (int): 当前trip的id
+    Returns:
+        area_id (int): 当前trip所在区县的id
+    """
+    cur_row = trip_df.iloc[trip_id]
+    if geometry.Point(cur_row['start_longitude'], cur_row['start_latitude']).within(geometry.shape(wuhou_shapes)) and geometry.Point(cur_row['end_longitude'], cur_row['end_latitude']).within(geometry.shape(wuhou_shapes)):
+        return wuhou_id,trip_id
+    elif geometry.Point(cur_row['start_longitude'], cur_row['start_latitude']).within(geometry.shape(jinjiang_shapes)) and geometry.Point(cur_row['end_longitude'], cur_row['end_latitude']).within(geometry.shape(jinjiang_shapes)):
+        return jinjiang_id,trip_id
+    else:
+      return -1,trip_id
+    
+# 主函数    
+if __name__ == '__main__':
+    # 筛选出武侯区和锦江区的数据,分别记为trip_df_wuhou和trip_df_jinjiang
+
+    # 遍历trip_df，将武侯区和锦江区的数据分别存入trip_df_wuhou和trip_df_jinjiang
+    start_time = time.time()
+    # 遍历每个trip,这一段可以用多进程优化
+    with ProcessPoolExecutor(max_workers=worker_num) as executor:
+        results = executor.map(process_trip_dis, range(len(trip_df)))
+        # futures = [executor.submit(process_trip_dis, trip_id) for trip_id in range(len(trip_df))]
+        # for future in as_completed(futures):
+        #     area_id,trip_id = future.result()
+        #     if area_id == wuhou_id:
+        #         trip_df_wuhou = pd.concat([trip_df_wuhou, trip_df.iloc[trip_id]], ignore_index=True)
+        #     elif area_id == jinjiang_id:
+        #         trip_df_jinjiang = pd.concat([trip_df_jinjiang, trip_df.iloc[trip_id]], ignore_index=True)
+    for area_id,trip_id in results:
+        if area_id == -1:
+            continue
+        # 以DataFrame取出一行数据
+        new_row = trip_df.iloc[trip_id]
+        # 将new_row添加到trip_df_wuhou或trip_df_jinjiang中
+        if area_id == wuhou_id:
+            trip_df_wuhou = trip_df_wuhou.append(new_row, ignore_index=True)
+        elif area_id == jinjiang_id:
+            trip_df_jinjiang = trip_df_jinjiang.append(new_row, ignore_index=True)
+            
+    end_time = time.time()
+    print('time cost: {:.2f}s'.format(end_time-start_time))
+
+    # 保存trip_df_wuhou和trip_df_jinjiang
+    trip_df_wuhou.to_csv('CD_shape/trip_df_wuhou.csv', index=False)
+    trip_df_jinjiang.to_csv('CD_shape/trip_df_jinjiang.csv', index=False)
---
+++ GPS_data/process/concur.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   concur.py
+@Time    :   2023/07/28
+@Author  :   ChenYiTao 
+@Version :   3.9.13 64-bit
+@Desc    :   并发技术提高程序运行效率
+'''
+
+# 模块导入
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from geopy.distance import geodesic
+import time
+import multiprocessing
+import warnings
+from math import radians, sin, cos, asin, sqrt
+
+
+warnings.filterwarnings('ignore')
+
+# 常量定义
+worker_num = 8  # 进程数可设为逻辑CPU数-1
+
+# 变量定义
+# 读取pkl文件，得到df_selected
+df_selected = pd.read_pickle('GPS_data\\df_selected.pkl')
+
+column_names = [
+    'taxi_id', 'passenger', 'start_time', 'end_time', 'start_latitude',
+    'start_longitude', 'end_latitude', 'end_longitude', 'distance'
+]
+# 当passenger列为1时，表示载客，为0时表示无客,以此来把轨迹GPS划分为载客轨迹和无客轨迹
+# 并记录载客轨迹和无客轨迹的起始时间和终止时间(s)，起始地点和终止地点、轨迹长度(km)(不是位移)
+trip_df = pd.DataFrame(columns=column_names)
+
+# 合并df_selected中连续的载客数据或无客数据，形成一条轨迹
+groups = [(cur_taxi, group)
+          for cur_taxi, group in df_selected.groupby('taxi_id')]
+
+
+# 函数定义
+# 定义一个函数，用来计算两个经纬度之间的距离
+# def get_distance(point1, point2):
+#     """
+#     计算两个经纬度之间的距离
+#     :param point1: 起点经纬度 (纬度, 经度)
+#     :param point2: 终点经纬度 (纬度, 经度)
+#     :return: 距离（km）
+#     """
+#     return geodesic(point1, point2).km
+
+def get_distance(lat1,lon1,lat2,lon2,):
+    # 将十进制转为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine公式
+    d_lon = lon2 - lon1
+    d_lat = lat2 - lat1
+    aa = sin(d_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
+    c = 2 * asin(sqrt(aa))
+    r = 6371  # 地球半径，千米
+    return c * r
+
+def process_group(cur_taxi, group):
+    """
+    处理一辆出租车的轨迹数据，将其划分为一条条轨迹，每条轨迹记录其载客情况、起始时间、终止时间、起始地点、终止地点、轨迹长度
+
+    Args:
+        cur_taxi (int): 当前出租车的id
+        group (DataFrame): 当前出租车的轨迹数据
+
+    Returns:
+        result_df (DataFrame): 当前出租车的处理结果，是该出租车的每一段(空驶、载客)轨迹的记录
+    """
+
+    result_df = pd.DataFrame(columns=column_names)
+    last_row = group.iloc[0].to_dict()
+    cur_passenger = last_row['passenger']
+    cur_distance = 0
+    start_latitude = last_row['latitude']
+    start_longitude = last_row['longitude']
+    start_time = last_row['time_s']
+
+    for i in range(1, group.shape[0]):  # 从第二行开始遍历
+        cur_row = group.iloc[i].to_dict()
+
+        if cur_passenger != cur_row['passenger']:
+            # 如果载客情况发生了变化则表示本段轨迹记录完成，记录其终止时间、终止地点
+            cur_distance += get_distance(
+                cur_row['latitude'], cur_row['longitude'],
+                last_row['latitude'], last_row['longitude'])
+            end_time = last_row['time_s']
+            end_latitude = last_row['latitude']
+            end_longitude = last_row['longitude']
+            new_row = pd.DataFrame({'taxi_id': cur_taxi, 'passenger': cur_passenger, 'start_time': start_time, 'end_time': end_time, \
+                                    'start_longitude': start_longitude, 'start_latitude': start_latitude,\
+                                    'end_longitude': end_longitude, 'end_latitude': end_latitude, 'distance': cur_distance},index=[0])
+            result_df = pd.concat([result_df, new_row], ignore_index=True)
+            # 重置轨迹的起始时间、起始地点、载客情况、轨迹长度
+            # 轨迹的起始时间
+            start_time = cur_row['time_s']
+            # 轨迹的起始地点
+            start_latitude = cur_row['latitude']
+            start_longitude = cur_row['longitude']
+            # 轨迹的载客情况
+            cur_passenger = cur_row['passenger']
+            # 轨迹的长度
+            cur_distance = 0
+        else:  # 如果载客情况没有发生变化，则添加轨迹长度，继续记录轨迹
+            cur_distance += get_distance(
+                cur_row['latitude'], cur_row['longitude'],
+                last_row['latitude'], last_row['longitude'])
+        last_row = cur_row
+
+    return result_df
+
+
+# 主函数
+if __name__ == '__main__':
+    print(df_selected.info(memory_usage='deep'))
+    time_start = time.time()
+    
+    # 使用spawn方式创建进程池，避免进程间共享资源的问题
+    # with ProcessPoolExecutor(
+    #         max_workers=worker_num,
+    #         mp_context=multiprocessing.get_context('spawn')) as executor:
+    #     results = executor.map(process_group,
+    #                            [cur_taxi for cur_taxi, group in groups],
+    #                            [group for cur_taxi, group in groups])
+    #     for result in results:
+    #         trip_df = pd.concat([trip_df, result], ignore_index=True)
+
+    with ProcessPoolExecutor(max_workers=worker_num) as executor:
+        futures = [executor.submit(process_group, cur_taxi, group) for cur_taxi, group in groups]
+        for future in as_completed(futures):
+            trip_df = pd.concat([trip_df, future.result()], ignore_index=True)
+
+    time_end = time.time()
+    trip_df.to_pickle('GPS_data\\trip_df.pkl')
+    print('time cost', time_end - time_start, 's')
